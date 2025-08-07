63313e2e83f787edd8f89cabd0f9a644acb847f6
IdokoMarcelina <aitmacelina@gmail.com>
2025-08-07 02:05:34 +0100

    deployed

--- onsite-program/submissions/week-4/day-4/day4-task-1/marcelina/contracts/School.sol
+++ onsite-program/submissions/week-4/day-4/day4-task-1/marcelina/contracts/School.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-//0x203B377f9a23aA9B18d34eB0047D40d7f37e2674
-
+//0xE292097a356666ff0C3b6De5895dA3d921B8ab88
 
 
 contract SchoolManagementSystem {
