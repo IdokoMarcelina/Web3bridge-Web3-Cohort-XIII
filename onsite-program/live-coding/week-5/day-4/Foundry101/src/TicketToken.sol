70668247177ceba9bbbfe9a7eb6bbf1eacc91267
IdokoMarcelina <aitmacelina@gmail.com>
2025-08-09 02:25:15 +0100

    event ticketting

--- onsite-program/live-coding/week-5/day-4/Foundry101/src/TicketToken.sol
+++ onsite-program/live-coding/week-5/day-4/Foundry101/src/TicketToken.sol
@@ -7,4 +7,8 @@ contract TicketToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Ticket", "TKT") {
         _mint(msg.sender, initialSupply);
     }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
 }
